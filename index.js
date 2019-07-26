d9b09c2618b35493e3459a767cb29c130b54f3a4
0x0a0d <vandug23@gmail.com>
2019-07-26 16:10:45 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Request = require('request-promise-native');
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'DNT': '1',
+};
+
+/**
+ * @param {request.CoreOptions} option
+ * @returns {request.RequestAPI<requestPromise.RequestPromise<any>, requestPromise.RequestPromiseOptions, request.RequiredUriUrl>}
+ */
+function createRequest(option = {}) {
+    if (option.headers != null) {
+        option.headers = {
+            ...headers,
+            ...option.headers
+        }
+    } else {
+        option.headers = headers;
+    }
+    return Request.defaults({
+        forever: true,
+        gzip: true,
+        simple: false,
+        resolveWithFullResponse: true,
+        timeout: 30 * 60 * 1000,
+        ...option
+    });
+}
+
+/**
+ * @type {{createRequest: (function(request.CoreOptions): request.RequestAPI<requestPromise.RequestPromise<any>, requestPromise.RequestPromiseOptions, request.RequiredUriUrl>)}}
+ */
+module.exports = {
+    createRequest
+};
